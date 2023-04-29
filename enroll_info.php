34a4a2625c9412e54cc4f1d539ea3ca31abd495e
jison-jpeg <2101101090@student.buksu.edu.ph>
2023-04-29 21:13:19 +0800

    added function that will hide the action bar

--- enroll_info.php
+++ enroll_info.php
@@ -1,10 +1,21 @@
+<?php
+	// Check if current page is index.php
+	if (basename($_SERVER['PHP_SELF']) != 'index.php') {
+		// Display action column on all other pages
+		$action_column = "<th scope='col'>Action</th>";
+	} else {
+		// Hide action column on index.php
+		$action_column = "";
+	}
+?>
+
 <table class="table table-borderless datatable">
   <thead>
     <tr>
       <th scope="col">#ID</th>
       <th scope="col">Student</th>
       <th scope="col">Age</th>
-      <th scope="col">Action</th>
+      <?php echo $action_column; ?>
     </tr>
   </thead>
   
@@ -23,7 +34,9 @@
 			echo "<td>" . $row['id'] . "</td>";
 			echo "<td>" . $row['name'] . "</td>";
 			echo "<td>" . $row['age'] . "</td>";
-			echo "<td><a href='edit.php?id=" . $row['id'] . "'><button type='button' class='btn btn-primary btn-sm' data-bs-toggle='modal' data-bs-target='#ExtralargeModal'>Edit</button></a> | <a href='delete_stud.php?id=" . $row['id'] . "' onclick='return confirm(\"Are you sure?\")'><button type='button' class='btn btn-primary btn-sm'>Delete</button></a></td>";
+			if (basename($_SERVER['PHP_SELF']) != 'index.php') {
+				echo "<td><a href='edit.php?id=" . $row['id'] . "'><button type='button' class='btn btn-primary btn-sm' data-bs-toggle='modal' data-bs-target='#ExtralargeModal'>Edit</button></a> | <a href='delete_stud.php?id=" . $row['id'] . "' onclick='return confirm(\"Are you sure?\")'><button type='button' class='btn btn-primary btn-sm'>Delete</button></a></td>";
+			}
 			echo "</tr>";
 		}
 
